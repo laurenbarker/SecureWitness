c1f0775b1da0ba08b55e36f5138e3d6ecbd160fb
Fangyang Cui <fc5fz@virginia.edu>
2015-03-29 19:19:36 -0400

    Added Search Page

--- SecureWitness/views.py
+++ SecureWitness/views.py
@@ -4,9 +4,8 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from SecureWitness.models import report,user
-from django.contrib.admin.widgets import AdminDateWidget
+from django.db.models import Q
 from django.contrib.admin import widgets
-import time
 
 #put forms in forms.py later
 from django import forms
@@ -16,13 +15,24 @@ class loginForm(forms.Form):
     Register = forms.BooleanField(required=False)
 
 class NameForm(forms.Form):
-    your_name = forms.CharField(label='Search Criteria', max_length=100)
+    desc = forms.CharField(label='Description Search Criteria', max_length=100)
+    loc_and = forms.BooleanField(required=False, label="AND")
+    loc_or = forms.BooleanField(required=False, label = "OR")
+    loc = forms.CharField(label="Location Search", max_length=50)
+    key_and = forms.BooleanField(required=False, label="AND")
+    key_or = forms.BooleanField(required=False, label = "OR")
+    key = forms.CharField(label="Keyword Search", max_length=50)
+    inc_and = forms.BooleanField(required=False, label="AND")
+    inc_or = forms.BooleanField(required=False, label = "OR")
+    inc_start = forms.DateField(label="Incident Start Date", widget=widgets.AdminDateWidget())
+    inc_end = forms.DateField(label="Incident End Date", widget=widgets.AdminDateWidget())
+
 
 class UploadFileForm(forms.Form):
     shortdesc = forms.CharField(max_length=50)
     longdesc = forms.CharField(max_length=300)
     location = forms.CharField(max_length=50, required=False)
-    incident_date = forms.DateField(required=False,widget = widgets.AdminDateWidget())
+    incident_date = forms.DateField(required=False, widget=widgets.AdminDateWidget())
     keywords = forms.CharField(max_length=50, required=False)
     private = forms.BooleanField(required=False)
     file = forms.FileField(required=False)
@@ -74,15 +84,52 @@ def search(request):
             # process the data in form.cleaned_data as required
             # ...
             # redirect to a new URL:
-            keyword = form.cleaned_data['your_name']
+            keyword = form.cleaned_data['desc']
             keyword = keyword.split()
-            list = []
+            desclist = []
+            loclist = []
+            keylist = []
             for word in keyword:
-                list.extend(report.objects.filter(shortdesc__contains=word))
-            list = set(list)
+                #list.extend(report.objects.filter(shortdesc__contains=word))
+                #list.extend(report.objects.filter(longdesc__contains=word))
+                qs =  report.objects.filter(Q(shortdesc__icontains=word) | Q(longdesc__icontains=word))
+                desclist.extend(qs)
+            desclist = set(desclist)
+            loc = form.cleaned_data['loc']
+            loc = loc.split()
+            for word in loc:
+                qs = report.objects.filter(Q(location__icontains=word))
+                loclist.extend(qs)
+            loclist = set(loclist)
+            key = form.cleaned_data['key']
+            key = key.split()
+            for word in key:
+                qs = report.objects.filter(Q(keywords__icontains=word))
+                keylist.extend(qs)
+            keylist = set(keylist)
+            inc_start = request.POST.get('inc_start')
+            inc_start = inc_start.split("/")
+            inc_start = inc_start[2] + "-" + inc_start[0] + "-" + inc_start[1]
+            inc_end = request.POST.get('inc_end')
+            inc_end = inc_end.split("/")
+            inc_end = inc_end[2] + "-" + inc_end[0] + "-" + inc_end[1]
+            inc_list = report.objects.filter(incident_date__range=(inc_start,inc_end))
+
+            if(form.cleaned_data['loc_and']):
+                desclist = list(set(desclist) & set(loclist))
+            else:
+                desclist = list(set(desclist) | set(loclist))
+            if(form.cleaned_data['key_and']):
+                desclist = list(set(desclist) & set(keylist))
+            else:
+                desclist = list(set(desclist) | set(keylist))
+            if(form.cleaned_data['inc_and']):
+                desclist = list(set(desclist) & set(inc_list))
+            else:
+                desclist = list(set(desclist) | set(inc_list))
             template = loader.get_template('SecureWitness/index.html')
             context = RequestContext(request, {
-                  'latest_question_list': list,
+                  'report_list': desclist,
             })
             return HttpResponse(template.render(context))
     # if a GET (or any other method) we'll create a blank form
@@ -101,7 +148,10 @@ def upload(request):
             if form.cleaned_data['incident_date'] is None:
                 inc = None
             else:
+                #convert date format to YYYY-MM-DD from MM/DD/YYYY
                 inc = request.POST.get('incident_date')
+                date = inc.split("/")
+                date = date[2] + "-" + date[0] + "-" + date[1]
             key = request.POST.get('keywords')
             priv = request.POST.get('private')
             if priv is None:
@@ -110,7 +160,7 @@ def upload(request):
             #Once login is finished, get current logged in user
             name = "test"
             u = user.objects.filter(username=name)[0]
-            rep = report(author = u, shortdesc = short, longdesc = long, location = loc, incident_date = inc, keywords = key, private = priv, file = f)
+            rep = report(author = u, shortdesc = short, longdesc = long, location = loc, incident_date = date, keywords = key, private = priv, file = f)
             rep.save()
             return HttpResponse("added successfully")
     else:
