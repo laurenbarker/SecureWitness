06fa975997635b634c121b260a1e9966e72eeb63
Lauren Barker <leb2dg@virginia.edu>
2015-04-12 19:34:05 -0400

    downloads input file name

--- decrypt.py
+++ decrypt.py
@@ -5,16 +5,20 @@ from Crypto.PublicKey import RSA
 from Crypto import Random
 from os.path import expanduser
 import sys
+import requests
 
 # user enters file to decrypt
-file_enc = input('Please enter the file you wish to decrypt: ')
+file_enc = raw_input("Please enter the file you wish to decrypt: ")
+print ('Your input was' + " " + file_enc)
 
+r = requests.get('http://localhost:8000/SecureWitness/uploaded_files/' + file_enc)
+print r.text
 
 # get key from db
 # read in file to enc_data
 
 # decrypt 
-print key.decrypt(enc_data) 
+#print key.decrypt(enc_data) 
 
 downloads = expanduser("~/Downloads/")
 
@@ -23,15 +27,3 @@ downloads = expanduser("~/Downloads/")
 
 # not sure we need below
 
-#sign
-text = 'abcdefgh'
-hash = SHA256.new(text).digest()
-print hash
-signature = key.sign(hash, '')
-print signature
-
-#verify
-text = 'abcdefgh'
-hash = SHA256.new(text).digest()
-print hash
-print public_key.verify(hash, signature)
