b29e32805c4363af2b6d60bbc752e37f7b6b5dee
Lauren Barker <leb2dg@virginia.edu>
2015-04-12 19:59:14 -0400

    added comments

--- decrypt.py
+++ decrypt.py
@@ -7,10 +7,18 @@ from os.path import expanduser
 import sys
 import requests
 
-# user enters file to decrypt
+# enter username
+
+# enter password
+
+# if logged in properly then show files available
+
+# user enters file to decrypt 
+# check if file is one displayed
 file_enc = raw_input("Please enter the file you wish to decrypt: ")
 print ('Your input was' + " " + file_enc)
 
+# gets file to decrpyt
 r = requests.get('http://localhost:8000/SecureWitness/uploaded_files/' + file_enc)
 print r.text
 
@@ -18,7 +26,7 @@ print r.text
 # read in file to enc_data
 
 # decrypt 
-#print key.decrypt(enc_data) 
+# print key.decrypt(enc_data) 
 
 downloads = expanduser("~/Downloads/")
 
