4a5def37f472b6713ca4be895b3f6dbd786c836b
Lauren Barker <leb2dg@virginia.edu>
2015-04-18 15:01:39 -0400

    added base for getting the key from db

--- SecureWitness/views.py
+++ SecureWitness/views.py
@@ -122,8 +122,29 @@ def viewFiles_decrypt(request):
         grp = grp + g
 
     #ob_list = report.objects.filter(reduce(lambda x, y: x | y, [Q(name__contains=word) for word in group_list]))
+    return HttpResponse(str(report_list) + grp)
+    #return HttpResponse(groups)
+
+@csrf_exempt
+def uploaded_key(request):
+    # get reports for user
+    name = request.POST.get('username')
+    u = user.objects.filter(username=name)[0]
+    report_list = report.objects.filter(Q(author=u) & (Q(folder = None) | Q(folder = "")))
+    groups = group.objects.all()
+    #create groups that have access to the report
+    group_list = []
+    for g in groups:
+        users = json.loads(g.users)
+        if u in users[g.groupName]:
+            group_list.append(g.groupName)
+    grp = ""
+    for g in group_list:
+        grp = grp + g
+
+    #ob_list = report.objects.filter(reduce(lambda x, y: x | y, [Q(name__contains=word) for word in group_list]))
     #return HttpResponse(str(report_list) + grp)
-    return HttpResponse(groups)
+    return HttpResponse("In progress...")
 
 def index(request):
     if 'u' in request.session:
