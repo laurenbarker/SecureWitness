0cc712249a174406f0351695e4fb154c69bdf295
Lauren Barker <leb2dg@virginia.edu>
2015-04-22 04:54:33 -0400

    added to settings

--- Team8/settings.py
+++ Team8/settings.py
@@ -84,7 +84,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+#STATIC_URL = '/static/'
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 MEDIA_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
