75334e86463930563a2339f7deba8f81fb30be0e
Lauren Barker <leb2dg@virginia.edu>
2015-04-23 16:33:10 -0400

    settings update

--- Team8/settings.py
+++ Team8/settings.py
@@ -10,10 +10,12 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-TEMPLATE_DIRS = ('%s/templates/'% (PROJECT_DIR),)
+# TEMPLATE_DIRS = ('%s/templates/'% (PROJECT_DIR),)
+
+TEMPLATE_DIRS = ['/templates/']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
