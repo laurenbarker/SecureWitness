837d630a86f0f0b227e595991fb9b85681d6aa9b
Lauren Barker <leb2dg@virginia.edu>
2015-04-26 20:30:45 -0400

    static url

--- Team8/settings.py
+++ Team8/settings.py
@@ -105,7 +105,7 @@ ALLOWED_HOSTS = ['*']
 # Static asset configuration
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = ''
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
